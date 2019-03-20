0e47ef4bff33d5f9d63d4ffd7f8fb46cd3b6af0a
Andrew Trick <andyjtrick@gmail.com>
2019-03-20 16:25:41 -0500

    starting up..

--- src/explore/time_to_hire_relations.py
+++ src/explore/time_to_hire_relations.py
@@ -5,7 +5,7 @@ Created on Wed Mar 20 15:27:10 2019
 @author: andyj
 """
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import os 
 
@@ -16,11 +16,39 @@ import os
 # setwd
 os.chdir('E:/projects/teradata/src/explore')
 # import
-df_contacts = pd.read_csv('../../data/processed/team_CAC/contact_hire_CAC.csv', encoding = "ISO-8859-1")
+df = pd.read_csv('../../data/processed/team_CAC/contact_hire_CAC.csv', encoding = "ISO-8859-1")
 
 # detailing df
-con_descript = df_contacts.describe() 
-df_contacts.info()
+con_descript = df.describe() 
+df.info()
 
 # col names
-headers = list(df_contacts.head(0))
+headers = list(df.head(0))
+
+# remove outliers
+df = df[(df['Days_in_Program'] < 400) & (df['Days_in_Program'] >= 0)]
+
+# check numeric correlations to dyas in program
+numeric_cols = ['Number_of_dependents__c', 'Min_Salary_Expectations__c', 'Salary_annual_expected', 
+                'Salary_hourly_expected', 'Age', 'Days_in_Program']
+df_num = df[numeric_cols]
+
+df_num_corr = df_num.corr()
+sns.heatmap(df_num_corr,
+            xticklabels=df_num_corr.columns,
+            yticklabels=df_num_corr.columns)
+
+sns.scatterplot(x = 'Age', y = 'Days_in_Program', data = df)
+
+sns.scatterplot(x = 'Min_Salary_Expectations__c', y = 'Days_in_Program', data = df)
+
+sns.scatterplot(x = 'Min_Salary_Expectations__c', y = 'Days_in_Program', hue = 'Gender__c', data = df)
+
+
+# RACE AND GENDER
+sns.countplot(x = 'Race__c', data = df)
+sns.boxplot(x = 'Gender__c', y = 'Days_in_Program', data = df)
+sns.barplot(x = 'Race__c', y = 'Days_in_Program', data = df)
+# THIS
+sns.violinplot(x = 'Race__c', y = 'Days_in_Program', hue = 'Gender__c', data = df, 
+               palette = 'muted', split = True, scale = 'count', inner = 'quartile')
