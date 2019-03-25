8b8841a698048c2336c1d943a038e74e945018c2
Andrew Trick <andyjtrick@gmail.com>
2019-03-25 07:09:29 -0500

    setup for modeling

---
+++ src/explore/modeling.R
@@ -0,0 +1,6 @@
+# log reg for hire likelyhood
+# reg for days in prog
+
+setwd('E:/projects/teradata/src/process/')
+df_topic_edit1 <- read.csv('../../data/processed/team_CAC/contact_hire_CAC.csv')
+
