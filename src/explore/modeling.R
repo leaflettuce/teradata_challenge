96933f35ba50e1cc3626576179e1da317c9123b4
Andrew Trick <andyjtrick@gmail.com>
2019-03-25 21:47:00 -0500

    regressions not useful :(

--- src/explore/modeling.R
+++ src/explore/modeling.R
@@ -8,7 +8,7 @@ df <- read.csv('../../data/processed/team_CAC/contact_hire_CAC.csv')
 # cut out unneeded cols
 df <- filter(df, Client__c == 1)
 
-keep <- c(1,10,11,16,17,18,40,42,43,65,74,81,106)
+keep <- c(10,11,18,40,65,74,81,106)
 df <- df[ , keep]
 
 # remove NAs
@@ -23,14 +23,55 @@ df <- df[-toBeRemoved,]
 toBeRemoved <- which(df$Gender__c=="")
 df <- df[-toBeRemoved,]
 
+
 #####################
 # set feature types #
 #####################
 
-####################
-# split test/train #
-####################
+y_time <- df$Days_in_Program
+y_hired <- df$Hire_Heroes_USA_Confirmed_Hire__c
+
+df$Days_in_Program <- NULL
+df$Hire_Heroes_USA_Confirmed_Hire__c <- NULL
+
+hh_workshop_temp <- df$HHUSA_Workshop_Participant__c
+df$HHUSA_Workshop_Participant__c <- NULL
+
+used_vol_temp <- df$Used_Volunteer_Services__c
+df$Used_Volunteer_Services__c <- NULL
+
+for(name in names(df)){
+  df[name] <- droplevels(df[name])
+}
+
+for(name in names(df)){
+  for(level in unique(df[name])){
+    df[paste("dummy", level, sep = "_")] <- ifelse(as.character(df[name]) == level, 1, 0)
+  }
+}
+
+
+df[c(1:5)] <- NULL
+df$Used_Volunteer <- 0
+df$Used_Volunteer <- used_vol_temp
+
+df$workshop <- hh_workshop_temp
 
 #########
 # model #
 #########
+
+df$days_in_program <- NULL
+df$hired <- y_hired
+
+# log reg
+log_model <- glm(formula = hired ~ ., family = binomial(link = "logit"), data = df)
+summary(log_model)
+
+
+# regression
+df$hired <- NULL
+df$Days_in_Program <- y_time
+
+reg <- lm(Days_in_Program ~ ., data = df)
+summary(reg)
