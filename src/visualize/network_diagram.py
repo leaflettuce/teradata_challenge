4fbee60e4f34b30462a6a2ffb202bb194ff996b0
Andrew Trick <andyjtrick@gmail.com>
2019-04-14 12:55:02 -0500

    cleaning started.

--- src/visualize/network_diagram.py
+++ src/visualize/network_diagram.py
@@ -6,6 +6,8 @@ Created on Sun Apr 14 12:00:00 2019
 """
 import os
 import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # setwd
 os.chdir('E:/projects/teradata/src/visualize/')
@@ -18,5 +20,25 @@ df.index
 
 # remove missing
 df = df.dropna()
-
 df.head()
+
+# clean up text
+skills = df.Qualifications
+for i in range(1,len(skills)): #iterate through and replace/split out
+    try:
+        skills[i] = skills[i].lower().replace(' and', '').replace(',', ' ').split(" ")
+    except KeyError:
+        pass
+
+
+
+# iterate through qualifications and track words
+skills_list = {}
+for section in df.Qualifications:
+    
+    for char in characters:
+        if char in section:
+            if str(iterative) in sections_dictionary.keys():
+                sections_dictionary[str(iterative)].append(char)  
+            else:
+sections_dictionary[str(iterative)] = [char] 
